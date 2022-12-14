2649488e8ebb7a667a8fa73e65ec1c871e9913fd
javi <francisco.magarino@ext.live.u-tad.com>
2022-12-14 21:43:46 +0100

    subo proyecto terminado a revisión

--- Practica4DAWE/src/areaTrabajo/MiPrograma.java
+++ Practica4DAWE/src/areaTrabajo/MiPrograma.java
@@ -4,6 +4,8 @@ public class MiPrograma {
 
 	public static void main(String[] args) {
 		// Preparación para el código
+		
+		System.out.println("Voy a hacer todo lo uqe mi jefe me pida :D");
 
 	}
 
